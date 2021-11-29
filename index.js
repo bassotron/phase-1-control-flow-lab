934b63eea229f83db0c7efce52508aead68f9b18
Will Lovell <willard.lovell@icloud.com>
2021-11-28 17:37:28 -0700

    commit tree 4ab1f9c677eb35a9fbfc179643efc1afd5f010ff

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+if (distance <= 400) {
+      return "This one is on me!";
+    }
+  else if (distance <= 2499 && distance >= 2000) {
+       return "I will gladly take your thirty bucks.";
+  }
+  else if (distance >= 2500) {
+    return "No can do.";
+  
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+   if (cityName === "NYC") {
+     return "Ok, sounds good."
+   }
+   else if (cityName = "Pittsburgh") {
+      return "No go."
+   
+   }
+    }
+
+   
+
+  
+function switchOnCharmFromTip(tipAmount){
+  if (tipAmount === "generous") {
+   return "Thank you so much."
+  }
+
+   else if (tipAmount === "not as generous") {
+        return "Thank you."
+}
+    else if (tipAmount === "thanks for everything") {
+      return "Bye."
+    }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
